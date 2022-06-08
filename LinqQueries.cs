f4bc31fa54fa2385a52316db323b89e1c302fcd6
Miguel Teheran <miguelteheran@gmail.com>
2022-06-08 16:55:33 -0500

    agregando importacion de datos

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,7 +1,18 @@
 public class LinqQueries
 {
+    private List<Book> librosCollection = new List<Book>();
+
     public LinqQueries()
     {
-        
+        using(StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+        }
+    }
+
+    public IEnumerable<Book> TodaLaColeccion()
+    {
+        return librosCollection;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach(var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
